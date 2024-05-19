da4aa4a5aa9f36e2dc2dc34f6ce78c81a14834b7
hemis23 <themischa9@gmail.com>
2024-05-19 08:12:16 +0300

    adding collaboration and services section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,8 @@
-function Heading({ className, title }) {
+function Heading({ className, title, text }) {
   return (
     <div className={`${className} max-50-[rem] mx-auto mb-12 lg:mg-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 }
